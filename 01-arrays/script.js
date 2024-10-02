d08ced15f0ff7fae77abd2b870a0e89baf27a1af
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-02 15:30:04 +0200

    fifth_test_completed

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -29,6 +29,7 @@ console.log(firstTeacher);
 
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa')
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
